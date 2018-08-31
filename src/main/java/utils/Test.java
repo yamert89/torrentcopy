6e5687ac12688e966db56309d55c2c4a2742d2be
yamert89 <38682240+yamert89@users.noreply.github.com>
2018-08-31 17:21:20 +0300

    Update Test.java

--- src/main/java/utils/Test.java
+++ src/main/java/utils/Test.java
@@ -31,8 +31,8 @@ public class Test {
 
             Jsoup.connect("http://login.free-torrents.org/forum/login.php")
                     .referrer("http://free-torrents.org/forum/indexer1.php")
-                    .data("login_username", "shurup7777")
-                    .data("login_password", "yamert89")
+                    .data("login_username", "***")
+                    .data("login_password", "***")
                     .timeout(40000)
                     //.cookies(response1.cookies())
                     .method(Connection.Method.POST)
